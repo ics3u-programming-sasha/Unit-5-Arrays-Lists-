2473028dc4ee7d384d1bda18a12210108f278ba5
Sasha Malko <sasha.malko@ocsbstudent.ca>
2019-11-26 13:07:35 -0500

    Worked on find max

---
+++ FindMaxSasha/FindMaxSasha/FindMaxForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace FindMaxSasha
+{
+    partial class frmFindMax
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lstValues = new System.Windows.Forms.ListBox();
+            this.lblMax = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(323, 35);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(86, 31);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // lstValues
+            // 
+            this.lstValues.FormattingEnabled = true;
+            this.lstValues.Location = new System.Drawing.Point(221, 117);
+            this.lstValues.Name = "lstValues";
+            this.lstValues.Size = new System.Drawing.Size(316, 212);
+            this.lstValues.TabIndex = 1;
+            // 
+            // lblMax
+            // 
+            this.lblMax.AutoSize = true;
+            this.lblMax.Font = new System.Drawing.Font("Lucida Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMax.Location = new System.Drawing.Point(308, 375);
+            this.lblMax.Name = "lblMax";
+            this.lblMax.Size = new System.Drawing.Size(18, 22);
+            this.lblMax.TabIndex = 3;
+            this.lblMax.Text = "?";
+            // 
+            // frmFindMax
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Wheat;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblMax);
+            this.Controls.Add(this.lstValues);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmFindMax";
+            this.Text = "Find Max Value by Sasha";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lstValues;
+        private System.Windows.Forms.Label lblMax;
+    }
+}
+
---
+++ FindMaxSasha/FindMaxSasha/FindMaxForm.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FindMaxSasha
+{
+    public partial class frmFindMax : Form
+    {
+
+        public frmFindMax()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            // declare local variables
+            const int MAX_ARRAY_SIZE = 10;
+            const int MAX_RANDOM_NUMBER = 500;
+            int[] arrayOfIntegers = new int[MAX_ARRAY_SIZE];
+            int counter, randomNumber, maxValue;
+            Random randomNumberGenerator = new Random();
+
+            // clear list box
+            this.lstValues.Items.Clear();
+
+            for (counter = 0; counter < MAX_RANDOM_NUMBER; counter++)
+            {
+                // generate a new random number between 1 and the MAX_ARRAY_SIZE
+                randomNumber = randomNumberGenerator.Next(1, MAX_ARRAY_SIZE + 1);
+
+                // assign the random number to cell at position "counter" in the array
+                arrayOfIntegers[counter] = randomNumber;
+
+                // add the random number to the list
+                this.lstValues.Items.Add(randomNumber);
+
+                // refresh the form to display the new item in the list box
+                this.Refresh();
+            }
+
+            maxValue = GetMaxValue(arrayOfIntegers);
+            this.lblMax.Text = "The max value is: " + maxValue; 
+        }
+
+        private int GetMaxValue(int[] tmpArrayOfIntegers)
+        {
+            int tmpMaxValue = -1;
+            int counter;
+
+            for (counter = 0; counter < 10; counter++)
+            {
+                if (tmpMaxValue > tmpArrayOfIntegers[counter])
+                {
+                    tmpMaxValue = tmpArrayOfIntegers[counter];
+                }
+            }
+            return tmpMaxValue;
+        }
+    }
+}
