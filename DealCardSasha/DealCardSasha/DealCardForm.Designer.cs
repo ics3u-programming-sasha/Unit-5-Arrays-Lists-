338152738ca1c14970cf6c3764af26c31c07f940
Sasha Malko <36854156+SashaMalko0810@users.noreply.github.com>
2019-12-03 16:19:14 -0800

    Worked on deal card

---
+++ DealCardSasha/DealCardSasha/DealCardForm.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace DealCardSasha
+{
+    partial class frmDealCard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picCard = new System.Windows.Forms.PictureBox();
+            this.picCard2 = new System.Windows.Forms.PictureBox();
+            this.picCard3 = new System.Windows.Forms.PictureBox();
+            this.picCard4 = new System.Windows.Forms.PictureBox();
+            this.picCard5 = new System.Windows.Forms.PictureBox();
+            this.picCard6 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picCard)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCard2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCard3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCard4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCard5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCard6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picCard
+            // 
+            this.picCard.Image = global::DealCardSasha.Properties.Resources.TwoS;
+            this.picCard.Location = new System.Drawing.Point(100, 28);
+            this.picCard.Name = "picCard";
+            this.picCard.Size = new System.Drawing.Size(135, 183);
+            this.picCard.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picCard.TabIndex = 0;
+            this.picCard.TabStop = false;
+            this.picCard.Click += new System.EventHandler(this.picCard_Click);
+            // 
+            // picCard2
+            // 
+            this.picCard2.Image = global::DealCardSasha.Properties.Resources.TwoS;
+            this.picCard2.Location = new System.Drawing.Point(315, 28);
+            this.picCard2.Name = "picCard2";
+            this.picCard2.Size = new System.Drawing.Size(135, 183);
+            this.picCard2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picCard2.TabIndex = 1;
+            this.picCard2.TabStop = false;
+            this.picCard2.Click += new System.EventHandler(this.picCard2_Click);
+            // 
+            // picCard3
+            // 
+            this.picCard3.Image = global::DealCardSasha.Properties.Resources.TwoS;
+            this.picCard3.Location = new System.Drawing.Point(527, 28);
+            this.picCard3.Name = "picCard3";
+            this.picCard3.Size = new System.Drawing.Size(135, 183);
+            this.picCard3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picCard3.TabIndex = 2;
+            this.picCard3.TabStop = false;
+            this.picCard3.Click += new System.EventHandler(this.picCard3_Click);
+            // 
+            // picCard4
+            // 
+            this.picCard4.Image = global::DealCardSasha.Properties.Resources.TwoS;
+            this.picCard4.Location = new System.Drawing.Point(100, 242);
+            this.picCard4.Name = "picCard4";
+            this.picCard4.Size = new System.Drawing.Size(135, 183);
+            this.picCard4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picCard4.TabIndex = 3;
+            this.picCard4.TabStop = false;
+            this.picCard4.Click += new System.EventHandler(this.picCard4_Click);
+            // 
+            // picCard5
+            // 
+            this.picCard5.Image = global::DealCardSasha.Properties.Resources.TwoS;
+            this.picCard5.Location = new System.Drawing.Point(315, 242);
+            this.picCard5.Name = "picCard5";
+            this.picCard5.Size = new System.Drawing.Size(135, 183);
+            this.picCard5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picCard5.TabIndex = 4;
+            this.picCard5.TabStop = false;
+            this.picCard5.Click += new System.EventHandler(this.picCard5_Click);
+            // 
+            // picCard6
+            // 
+            this.picCard6.Image = global::DealCardSasha.Properties.Resources.TwoS;
+            this.picCard6.Location = new System.Drawing.Point(527, 242);
+            this.picCard6.Name = "picCard6";
+            this.picCard6.Size = new System.Drawing.Size(135, 183);
+            this.picCard6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picCard6.TabIndex = 5;
+            this.picCard6.TabStop = false;
+            this.picCard6.Click += new System.EventHandler(this.picCard6_Click);
+            // 
+            // frmDealCard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.picCard6);
+            this.Controls.Add(this.picCard5);
+            this.Controls.Add(this.picCard4);
+            this.Controls.Add(this.picCard3);
+            this.Controls.Add(this.picCard2);
+            this.Controls.Add(this.picCard);
+            this.Name = "frmDealCard";
+            this.Text = "Deal Card by Sasha";
+            ((System.ComponentModel.ISupportInitialize)(this.picCard)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCard2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCard3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCard4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCard5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCard6)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picCard;
+        private System.Windows.Forms.PictureBox picCard2;
+        private System.Windows.Forms.PictureBox picCard3;
+        private System.Windows.Forms.PictureBox picCard4;
+        private System.Windows.Forms.PictureBox picCard5;
+        private System.Windows.Forms.PictureBox picCard6;
+    }
+}
+
